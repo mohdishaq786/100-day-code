068a69f2064380f1eafe8e7c82938a6ac89d361a
ishaq <49558047+mohdishaq786@users.noreply.github.com>
2021-03-30 22:58:01 +0530

    bug life

--- spoj.cpp
+++ spoj.cpp
@@ -1,50 +1,97 @@
-#include<bits/stdc++.h>
 
+
+//BUGLIFE - A Bug’s Life
+
+#include<bits/stdc++.h>
+#define ll         long long int
+#define pii        pair<ll ,ll>
 #define pb         push_back
 #define vi         vector<int>
+#define vii        vector<ll>
+#define vs         vector<string>
+#define ump        unordered_map
+#define all(n)     n.begin(),n.end()
+#define ff         first
+#define ss         second
+#define mid(l,r)   (l+(r-1)/2)
+#define fo(i,n)    for(i=0;i<n;i++)
+#define endl       "\n"
+#define inf        1e18
 
+#define deb(x)     cout<< #x << " "<<x<<endl;
+#define w(c)       int c ; cin>>c; while(c);
 using namespace std;
 
 
-vi arr[1001];
-int vist[1001];
-
-void dfs(int node)
+vi arr[2001];
+int vist[2001], col[2001];
+void fast()
 {
-	vist[node] = 1;
-	for (auto& child : arr[node])
-	{
-		if (vist[child] == 0)
-			dfs(child);
-	}
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
 }
 
+bool dfs(int node, int c)
+{
+	vist[node] = 1;
+	col[node] = c;
+	for (int child : arr[node])
+	{
+		if (vist[child] == 0)
+		{
+			bool res = dfs(child, c ^ 1);
+			if (res == false)
+				return false;
+		}
+		else if (col[node]  == col[child])
+			return false;
 
-
-
-
+	}
+	return true;
+}
 
 int main()
 {
-
-	int n, m, a, b;
-	cin >> n >> m;
-	int ccount = 0;
-	for (int i = 1; i <= m; i++)
+	fast();
+	int t, n, m, a, b;
+	cin >> t;
+	for (int tc = 1; tc <= t; tc++)
 	{
-		cin >> a >> b;
-		arr[a].pb(b);
-		arr[b].pb(a);
+		cin >> n >> m;
+		for (int i = 1; i <= n; i++) arr[i].clear(), vist[i] = 0;
 
+		for (int i = 1; i <= m; i++)
+			cin >> a >> b, arr[a].pb(b), arr[b].pb(a);
+
+		bool flag = true;
+		for (int i = 1; i <= n; i++)
+		{
+			if (vist[i] == 0)
+			{
+				bool result = dfs(i, 0);
+				if (result == false)
+					flag = false;
+			}
+
+		}
+		cout << "Scenario #" << tc << ":" << endl;
+
+		if (flag == false)
+			cout << "Suspicious bugs found!" << endl;
+		else
+			cout << "No suspicious bugs found!" << endl;
 	}
-	for (int i = 1; i <= n; i++)
-		if (vist[i] == 0)
-			dfs(i); ccount++;
+	return 0;
 
 
-	if (ccount == 1 && m == n - 1)
-		cout << "YES";
-	else
-		cout << "NO";
+
+
+
 
 }
